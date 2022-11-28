91de608c7975a5d095ed51f8342ce6040f94723b
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-11-28 20:53:52 +0400

    improve query

--- sql_queries/customers_activity/extract/tickets_with_iterations.sql
+++ sql_queries/customers_activity/extract/tickets_with_iterations.sql
@@ -1,5 +1,7 @@
 DECLARE @start_date DATE = '{start_date}'
 DECLARE @end_date DATE = '{end_date}';
+
+
 WITH ticket_tags AS (
 	SELECT
 		Tickets AS tickets,
@@ -27,11 +29,12 @@ FROM
 		SELECT 	COUNT(TicketId) AS iterations
 		FROM  	DXStatisticsV2.dbo.IterationItems AS ii
 		WHERE 	TicketId = ti.Id ) AS ii
-	INNER JOIN DXStatisticsV2.dbo.Users AS u ON u.Id = ti.OwnerGuid
+	INNER JOIN SupportCenterPaid.[c1f0951c-3885-44cf-accb-1a390f34c342].Users AS u ON u.Id = ti.OwnerGuid
+	INNER JOIN CRM.dbo.Customers AS crmCustomer ON crmCustomer.FriendlyId = u.FriendlyId
 	CROSS APPLY(
 		SELECT 	STRING_AGG(CONVERT(NVARCHAR(MAX), UserGroup_Id), ' ') AS groups
 		FROM 	CRM.dbo.Customer_UserGroup
-		WHERE 	Customer_Id = u.CRMid ) AS ug
+		WHERE 	Customer_Id = crmCustomer.Id ) AS ug
 	LEFT JOIN DXStatisticsV2.dbo.TribeTeamMapping AS ttm ON ttm.SupportTeam = ISNULL(ti.ProcessingSupportTeam, ti.SupportTeam)
 	INNER JOIN CRM.dbo.Tribes AS tribes ON ttm.Tribe = tribes.Id
 	LEFT JOIN ticket_tags AS tt ON tt.tickets = ti.Id
