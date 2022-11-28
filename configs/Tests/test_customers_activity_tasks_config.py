c532cb22228a40910ec0ba6b8271e4cebcb1f6f9
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-11-28 20:23:40 +0400

    decrease period

--- configs/Tests/test_customers_activity_tasks_config.py
+++ configs/Tests/test_customers_activity_tasks_config.py
@@ -1,10 +1,9 @@
 from configs.customers_activity_tasks_config import CustomersActivityTasksConfig
 from datetime import date, datetime
-from dateutil.relativedelta import relativedelta
 
 
 def test_get_tickets_with_iterations_period():
-    end = date.today() + relativedelta(days=1)
+    end = date.today()
     start = datetime(end.year - 2, 1, 1)
     assert CustomersActivityTasksConfig.get_tickets_with_iterations_period() == {
         'start_date': start.strftime('%Y-%m-%d'),
--- configs/customers_activity_tasks_config.py
+++ configs/customers_activity_tasks_config.py
@@ -1,5 +1,4 @@
 from datetime import date, datetime
-from dateutil.relativedelta import relativedelta
 from toolbox.utils.converters import DateTimeToSqlString
 
 
@@ -7,7 +6,7 @@ class CustomersActivityTasksConfig:
 
     @staticmethod
     def get_tickets_with_iterations_period() -> dict[str, str]:
-        end = date.today() + relativedelta(days=1)
+        end = date.today()
         start = datetime(end.year - 2, 1, 1)
         return {
             'start_date': DateTimeToSqlString.convert(start, separator='-'),
