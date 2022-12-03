4ccaa9c75211874848c04e35de72873e3530d618
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-03 20:30:30 +0400

    refactor filter generation

--- repository/customers_activity/local/sql_query_params_generator.py
+++ repository/customers_activity/local/sql_query_params_generator.py
@@ -1,54 +1,5 @@
-from typing import Callable, Any
+from toolbox.sql.generators.filter_clause_generator import SqlFilterClauseGenerator
 from sql_queries.customers_activity.meta import TicketsWithIterationsMeta
-
-
-class SqlFilterClauseGenerator:
-
-    def generate_in_filter(
-        self,
-        col: str,
-        values: list,
-        filter_prefix: str,
-        values_converter: Callable[[Any], str],
-    ) -> str:
-
-        def filter_func():
-            res = f'{col} IN ('
-            res += ','.join([values_converter(val) for val in values])
-            res += ')'
-            return res
-
-        return self._generate_filter(
-            values=values, filter_prefix=filter_prefix, get_filter=filter_func
-        )
-
-    def generate_like_filter(
-        self,
-        col: str,
-        values: list,
-        filter_prefix: str,
-    ):
-
-        def filter_func():
-            res = '('
-            res += ' OR '.join([f"{col} LIKE '%{value}%'" for value in values])
-            res += ')'
-            return res
-
-        return self._generate_filter(
-            values=values, filter_prefix=filter_prefix, get_filter=filter_func
-        )
-
-    def _generate_filter(
-        self,
-        values: list,
-        filter_prefix: str,
-        get_filter: Callable[[], str],
-    ) -> str:
-        if not values:
-            return ''
-        actual_filter = get_filter()
-        return filter_prefix + actual_filter
 
 
 class TicketsWithIterationsAggregatesSqlFilterClauseGenerator:
