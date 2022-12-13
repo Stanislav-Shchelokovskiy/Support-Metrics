81bd34666395d37fb9d4cc82b99bf4809f27cbd2
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2022-12-13 12:37:20 +0400

    format what returned by get_tickets_with_iterations_raw

--- configs/customers_activity_tasks_config.py
+++ configs/customers_activity_tasks_config.py
@@ -7,7 +7,7 @@ class CustomersActivityTasksConfig:
     @staticmethod
     def get_tickets_with_iterations_period() -> dict[str, str]:
         end = date.today()
-        start = datetime(end.year - 1, 1, 1)
+        start = datetime(end.year - 4, 1, 1)
         return {
             'start_date': DateTimeToSqlString.convert(start, separator='-'),
             'end_date': DateTimeToSqlString.convert(end, separator='-'),
