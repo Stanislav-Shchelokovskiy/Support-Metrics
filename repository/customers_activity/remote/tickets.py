aa3f5c489337a854bfa59555083fba01e0d9f5af
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-01-25 18:35:24 +0400

    create tickets types repository

--- repository/customers_activity/remote/tickets.py
+++ repository/customers_activity/remote/tickets.py
@@ -1,6 +1,9 @@
 from toolbox.sql.repository import Repository
 from sql_queries.index import CustomersActivitySqlPathIndex
-from sql_queries.customers_activity.meta import TicketsTagsMeta
+from sql_queries.customers_activity.meta import (
+    TicketsTagsMeta,
+    TicketsTypesMeta,
+)
 
 
 class TicketsTagsRepository(Repository):
@@ -16,3 +19,18 @@ class TicketsTagsRepository(Repository):
 
     def get_must_have_columns(self, kwargs: dict) -> list[str]:
         return TicketsTagsMeta.get_values()
+
+
+class TicketsTypesRepository(Repository):
+    """
+    Loads tickets types.
+    """
+
+    def get_main_query_path(self, kwargs: dict) -> str:
+        return CustomersActivitySqlPathIndex.get_tickets_types_path()
+
+    def get_main_query_format_params(self, kwargs: dict) -> dict[str, str]:
+        return TicketsTypesMeta.get_attrs()
+
+    def get_must_have_columns(self, kwargs: dict) -> list[str]:
+        return TicketsTypesMeta.get_values()
