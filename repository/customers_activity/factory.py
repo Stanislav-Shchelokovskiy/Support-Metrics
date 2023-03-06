fb103aba7ac57393de3dbac82aee49e167fff753
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-03-06 19:18:16 +0400

    refactor

--- repository/customers_activity/factory.py
+++ repository/customers_activity/factory.py
@@ -25,55 +25,55 @@ class SqlServerFactory:
     def _create_repository(self, queries: RepositoryQueries) -> SqlServerRepository:
         return SqlServerRepository(queries=queries)
 
-    def create_groups_repository(self, **kwargs) -> SqlServerRepository:
+    def create_groups_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=customers_groups.CustomersGroups())
 
-    def create_tracked_groups_repository(self, **kwargs) -> SqlServerRepository:
+    def create_tracked_groups_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=customers_groups.TrackedCustomersGroups())
 
-    def create_tags_repository(self, **kwargs) -> SqlServerRepository:
+    def create_tags_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.TicketsTags())
 
-    def create_replies_types_repository(self, **kwargs) -> SqlServerRepository:
+    def create_replies_types_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=cat.CATRepliesTypes())
 
-    def create_components_features_repository(self, **kwargs) -> SqlServerRepository:
+    def create_components_features_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=cat.CATComponentsFeatures())
 
-    def create_customers_tickets_repository(self, **kwargs) -> SqlServerRepository:
+    def create_customers_tickets_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets_and_iterations.CustomersTickets())
 
-    def create_license_statuses_repository(self, **kwargs) -> Repository:
+    def create_license_statuses_repository(self) -> Repository:
         return licenses_conversion.LicenseStatusesRepository()
 
-    def create_conversion_statuses_repository(self, **kwargs) -> Repository:
+    def create_conversion_statuses_repository(self) -> Repository:
         return licenses_conversion.ConversionStatusesRepository()
 
-    def create_platforms_products_repository(self, **kwargs) -> SqlServerRepository:
+    def create_platforms_products_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=platforms_products.PlatformsProducts())
 
-    def create_employees_iterations_repository(self, **kwargs) -> SqlServerRepository:
+    def create_employees_iterations_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets_and_iterations.EmployeesIterations())
 
-    def create_tickets_types_repository(self, **kwargs) -> SqlServerRepository:
+    def create_tickets_types_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.TicketsTypes())
 
-    def create_frameworks_repository(self, **kwargs) -> SqlServerRepository:
+    def create_frameworks_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.Frameworks())
 
-    def create_operating_systems_repository(self, **kwargs) -> SqlServerRepository:
+    def create_operating_systems_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.OperatingSystems())
 
-    def create_builds_repository(self, **kwargs) -> SqlServerRepository:
+    def create_builds_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.Builds())
 
-    def create_severity_repository(self, **kwargs) -> SqlServerRepository:
+    def create_severity_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.Severity())
 
-    def create_ticket_statuses_repository(self, **kwargs) -> SqlServerRepository:
+    def create_ticket_statuses_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.TicketStatuses())
 
-    def create_ides_repository(self, **kwargs) -> SqlServerRepository:
+    def create_ides_repository(self) -> SqlServerRepository:
         return self._create_repository(queries=tickets.IDEs())
 
 
@@ -81,85 +81,85 @@ class SqliteFactory:
     def _create_repository(self, queries: RepositoryQueries) -> SqliteRepository:
         return SqliteRepository(queries=queries)
 
-    def create_tickets_with_iterations_period_repository(self, **kwargs) -> SqliteRepository:
+    def create_tickets_with_iterations_period_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets_with_iterations.TicketsPeriod())
 
-    def create_customers_groups_repository(self, **kwargs) -> SqliteRepository:
+    def create_customers_groups_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_customers.CustomersGroups())
 
-    def create_tracked_customers_groups_repository(self, **kwargs) -> SqliteRepository:
+    def create_tracked_customers_groups_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_customers.TrackedCustomersGroups())
 
-    def create_tickets_types_repository(self, **kwargs) -> SqliteRepository:
+    def create_tickets_types_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.TicketsTypes())
 
-    def create_tickets_tags_repository(self, **kwargs) -> SqliteRepository:
+    def create_tickets_tags_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.TicketsTags())
 
-    def create_reply_types_repository(self, **kwargs) -> SqliteRepository:
+    def create_reply_types_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_cat.CATRepliesTypes())
 
-    def create_frameworks_repository(self, **kwargs) -> SqliteRepository:
+    def create_frameworks_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.Frameworks())
 
-    def create_operating_systems_repository(self, **kwargs) -> SqliteRepository:
+    def create_operating_systems_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.OperatingSystems())
 
-    def create_builds_repository(self, **kwargs) -> SqliteRepository:
+    def create_builds_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.BuildsRepositoryQueries())
 
-    def create_severity_repository(self, **kwargs) -> SqliteRepository:
+    def create_severity_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.Severity())
 
-    def create_ticket_statuses_repository(self, **kwargs) -> SqliteRepository:
+    def create_ticket_statuses_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.TicketStatuses())
 
-    def create_ides_repository(self, **kwargs) -> SqliteRepository:
+    def create_ides_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets.IDEs())
 
-    def create_license_statuses_repository(self, **kwargs) -> SqliteRepository:
+    def create_license_statuses_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_licenses_conversion.LicenseStatuses())
 
-    def create_conversion_statuses_repository(self, **kwargs) -> SqliteRepository:
+    def create_conversion_statuses_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_licenses_conversion.ConversionStatuses())
 
-    def create_platforms_repository(self, **kwargs) -> SqliteRepository:
+    def create_platforms_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_platforms_products.Platforms())
 
-    def create_products_repository(self, **kwargs) -> SqliteRepository:
+    def create_products_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_platforms_products.Products())
 
-    def create_components_repository(self, **kwargs) -> SqliteRepository:
+    def create_components_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_cat.CATComponents())
 
-    def create_features_repository(self, **kwargs) -> SqliteRepository:
+    def create_features_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_cat.CATFeatures())
 
-    def create_tickets_with_iterations_raw_repository(self, **kwargs) -> SqliteRepository:
+    def create_tickets_with_iterations_raw_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets_with_iterations.TicketsWithIterationsRaw())
 
-    def create_tickets_with_iterations_aggregates_repository(self, **kwargs) -> SqliteRepository:
+    def create_tickets_with_iterations_aggregates_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_tickets_with_iterations.TicketsWithIterationsAggregates())
 
-    def create_emp_positions_repository(self, **kwargs) -> SqliteRepository:
+    def create_emp_positions_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_employees.EmpPositions())
 
-    def create_emp_tribes_repository(self, **kwargs) -> SqliteRepository:
+    def create_emp_tribes_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_employees.EmpTribes())
 
-    def create_employees_repository(self, **kwargs) -> SqliteRepository:
+    def create_employees_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_employees.Employees())
 
-    def create_customers_repository(self, **kwargs) -> SqliteRepository:
+    def create_customers_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_customers.Customers())
 
-    def create_customers_validation_repository(self, **kwargs) -> SqliteRepository:
+    def create_customers_validation_repository(self) -> SqliteRepository:
         return self._create_repository(queries=sqlite_customers.CustomersValidation())
 
-    def create_display_filter_generator(self, **kwargs) -> DisplayFilterGenerator:
+    def create_display_filter_generator(self) -> DisplayFilterGenerator:
         return DisplayFilterGenerator
 
-    def create_periods_generator(self, **kwargs) -> PeriodsGenerator:
+    def create_periods_generator(z) -> PeriodsGenerator:
         return PeriodsGenerator
 # yapf: enable
 
