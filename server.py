de5604d6382e499d537082a26cf9962d2a47295d
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-05-29 14:08:44 +0400

    rename apis and extract common server models

--- server.py
+++ server.py
@@ -1,26 +1,23 @@
 import os
-import hashlib
 import help.index as help_index
 import repository.server_repository as server_repository
+import toolbox.cache.view_state_cache as view_state_cache
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import Response
 from collections.abc import Coroutine
 
 from toolbox.utils.converters import JSON_to_object
-from server_cache import ServerCache
 from server_models import (
     TicketsWithIterationsParams,
     TentsParams,
     FeatureParams,
-    StatAppState,
+    ViewState,
     ConversionStatusParams,
     EmployeeParams,
     CustomersParams,
 )
 
-
-server_cache = ServerCache()
 
 app = FastAPI()
 
@@ -47,138 +44,138 @@ async def get_repsonse_async(task: Coroutine):
 
 
 # yapf: disable
-@app.get('/get_tickets_with_iterations_period')
-async def customers_activity_get_tickets_with_iterations_period():
+@app.get('/TicketsWithIterationsPeriod')
+async def get_tickets_with_iterations_period():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_tickets_with_iterations_period()
+        server_repository.get_tickets_with_iterations_period()
     )
 
 
-@app.get('/get_customers_groups')
-async def customers_activity_get_customers_groups(tracked: bool = False):
+@app.get('/CustomersGroups')
+async def get_customers_groups(tracked: bool = False):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_tracked_customers_groups()
+        server_repository.get_tracked_customers_groups()
         if tracked else
-        server_repository.customers_activity_get_customers_groups()
+        server_repository.get_customers_groups()
     )
 
 
-@app.get('/get_tickets_types')
-async def customers_activity_get_tickets_types():
+@app.get('/TicketsTypes')
+async def get_tickets_types():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_tickets_types()
+        server_repository.get_tickets_types()
     )
 
 
-@app.get('/get_frameworks')
-async def customers_activity_get_frameworks():
+@app.get('/Frameworks')
+async def get_frameworks():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_frameworks()
+        server_repository.get_frameworks()
     )
 
 
-@app.get('/get_operating_systems')
-async def customers_activity_get_operating_systems():
+@app.get('/OperatingSystems')
+async def get_operating_systems():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_operating_systems()
+        server_repository.get_operating_systems()
     )
 
 
-@app.get('/get_builds')
-async def customers_activity_get_builds():
+@app.get('/Builds')
+async def get_builds():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_builds()
+        server_repository.get_builds()
     )
 
 
-@app.get('/get_severity_values')
-async def customers_activity_get_severity_values():
+@app.get('/SeverityValues')
+async def get_severity_values():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_severity_values()
+        server_repository.get_severity_values()
     )
 
 
-@app.get('/get_ticket_statuses')
-async def customers_activity_get_ticket_statuses():
+@app.get('/TicketStatuses')
+async def get_ticket_statuses():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_ticket_statuses()
+        server_repository.get_ticket_statuses()
     )
 
 
-@app.get('/get_ides')
-async def customers_activity_get_ides():
+@app.get('/Ides')
+async def get_ides():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_ides()
+        server_repository.get_ides()
     )
 
 
-@app.get('/get_tickets_tags')
-async def customers_activity_get_tickets_tags():
+@app.get('/TicketsTags')
+async def get_tickets_tags():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_tickets_tags()
+        server_repository.get_tickets_tags()
     )
 
 
-@app.get('/get_replies_types')
-async def customers_activity_get_cat_get_replies_types():
+@app.get('/CATRepliesTypes')
+async def get_cat_get_replies_types():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_replies_types()
+        server_repository.get_cat_replies_types()
     )
 
 
-@app.post('/get_components')
-async def customers_activity_get_cat_components(params: TentsParams):
+@app.post('/CATComponents')
+async def get_cat_components(params: TentsParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_components(
+        server_repository.get_cat_components(
             tent_ids=params.tents,
         )
     )
 
 
-@app.post('/get_features')
-async def customers_activity_get_cat_features(params: FeatureParams):
+@app.post('/CATFeatures')
+async def get_cat_features(params: FeatureParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_features(
+        server_repository.get_cat_features(
             tent_ids=params.tents,
             component_ids=params.components,
         )
     )
 
 
-@app.post('/get_platforms')
-async def customers_activity_get_platforms(params: TentsParams):
+@app.post('/Platforms')
+async def get_platforms(params: TentsParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_platforms(
+        server_repository.get_platforms(
             tent_ids=params.tents,
         )
     )
 
 
-@app.post('/get_products')
-async def customers_activity_get_products(params: TentsParams):
+@app.post('/Products')
+async def get_products(params: TentsParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_products(
+        server_repository.get_products(
             tent_ids=params.tents,
         )
     )
 
 
-@app.get('/get_group_by_periods')
-async def customers_activity_get_group_by_periods():
+@app.get('/GroupByPeriods')
+async def get_group_by_periods():
     return await get_repsonse_async(
-        server_repository.customers_activity_get_group_by_periods()
+        server_repository.get_group_by_periods()
     )
 
 
 # yapf: enable
-@app.get('/get_periods_array')
-async def customers_activity_get_periods_array(
+@app.get('/PeriodsArray')
+async def get_periods_array(
     start: str,
     end: str,
     format: str,
 ):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_periods_array(
+        server_repository.get_periods_array(
             start=start,
             end=end,
             format=format,
@@ -186,49 +183,39 @@ async def customers_activity_get_periods_array(
     )
 
 
-@app.get('/get_license_statuses')
-async def customers_activity_get_license_statuses():
-    return await get_repsonse_async(
-        server_repository.customers_activity_get_license_statuses()
-    )
+@app.get('/LicenseStatuses')
+async def get_license_statuses():
+    return await get_repsonse_async(server_repository.get_license_statuses())
 
 
-@app.post('/get_conversion_statuses')
-async def customers_activity_get_conversion_statuses(
-    params: ConversionStatusParams
-):
+@app.post('/ConversionStatuses')
+async def get_conversion_statuses(params: ConversionStatusParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_conversion_statuses(
+        server_repository.get_conversion_statuses(
             license_status_ids=params.license_statuses
         )
     )
 
 
-@app.get('/get_positions')
-async def customers_activity_get_emp_positions():
+@app.get('/Positions')
+async def get_emp_positions():
+    return await get_repsonse_async(server_repository.get_emp_positions())
+
+
+@app.get('/EmpTribes')
+async def get_emp_tribes():
+    return await get_repsonse_async(server_repository.get_emp_tribes())
+
+
+@app.get('/EmpTents')
+async def get_emp_tents():
+    return await get_repsonse_async(server_repository.get_emp_tents())
+
+
+@app.post('/Employees')
+async def get_employees(params: EmployeeParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_emp_positions()
-    )
-
-
-@app.get('/get_emp_tribes')
-async def customers_activity_get_emp_tribes():
-    return await get_repsonse_async(
-        server_repository.customers_activity_get_emp_tribes()
-    )
-
-
-@app.get('/get_emp_tents')
-async def customers_activity_get_emp_tents():
-    return await get_repsonse_async(
-        server_repository.customers_activity_get_emp_tents()
-    )
-
-
-@app.post('/get_employees')
-async def customers_activity_get_employees(params: EmployeeParams):
-    return await get_repsonse_async(
-        server_repository.customers_activity_get_employees(
+        server_repository.get_employees(
             position_ids=params.positions,
             tribe_ids=params.tribes,
             tent_ids=params.tents,
@@ -236,15 +223,15 @@ async def customers_activity_get_employees(params: EmployeeParams):
     )
 
 
-@app.post('/get_customers')
-async def customers_activity_get_customers(
+@app.post('/Customers')
+async def get_customers(
     params: CustomersParams,
     search: str = '',
     skip: int = 0,
     take: int = 0,
 ):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_customers(
+        server_repository.get_customers(
             filter_values=params.customers,
             search=search,
             skip=skip,
@@ -253,17 +240,15 @@ async def customers_activity_get_customers(
     )
 
 
-@app.post('/validate_customers')
-async def customers_activity_validate_customers(params: CustomersParams):
+@app.post('/ValidateCustomers')
+async def validate_customers(params: CustomersParams):
     return await get_repsonse_async(
-        server_repository.customers_activity_validate_customers(
-            values=params.customers
-        )
+        server_repository.validate_customers(values=params.customers)
     )
 
 
-@app.post('/get_tickets_with_iterations_aggregates')
-async def customers_activity_get_tickets_with_iterations_aggregates(
+@app.post('/TicketsWithIterationsAggregates')
+async def get_tickets_with_iterations_aggregates(
     group_by_period: str,
     range_start: str,
     range_end: str,
@@ -271,8 +256,7 @@ async def customers_activity_get_tickets_with_iterations_aggregates(
     body: TicketsWithIterationsParams,
 ):
     return await get_repsonse_async(
-        server_repository.
-        customers_activity_get_tickets_with_iterations_aggregates(
+        server_repository.get_tickets_with_iterations_aggregates(
             group_by_period=group_by_period,
             range_start=range_start,
             range_end=range_end,
@@ -282,15 +266,15 @@ async def customers_activity_get_tickets_with_iterations_aggregates(
     )
 
 
-@app.post('/get_tickets_with_iterations_raw')
-async def customers_activity_get_tickets_with_iterations_raw(
+@app.post('/TicketsWithIterationsRaw')
+async def get_tickets_with_iterations_raw(
     range_start: str,
     range_end: str,
     baseline_aligned_mode_enabled: bool,
     body: TicketsWithIterationsParams,
 ):
     return await get_repsonse_async(
-        server_repository.customers_activity_get_tickets_with_iterations_raw(
+        server_repository.get_tickets_with_iterations_raw(
             range_start=range_start,
             range_end=range_end,
             use_baseline_aligned_mode=baseline_aligned_mode_enabled,
@@ -299,31 +283,23 @@ async def customers_activity_get_tickets_with_iterations_raw(
     )
 
 
-@app.post('/get_customers_activity_display_filter')
-async def get_customers_activity_display_filter(
-    body: TicketsWithIterationsParams
-):
-    return await get_repsonse_async(
-        server_repository.customers_activity_get_display_filter(body)
-    )
+@app.post('/DisplayFilter')
+async def get_display_filter(body: TicketsWithIterationsParams):
+    return await get_repsonse_async(server_repository.get_display_filter(body))
 
 
-@app.get('/get_customers_activity_help')
-async def get_customers_activity_help():
-    return await get_repsonse_async(
-        help_index.get_customers_activity_descriptions()
-    )
+@app.get('/Help')
+async def get_help():
+    return await get_repsonse_async(help_index.get_descriptions())
 
 
-@app.post('/push_state')
-def push_state(params: StatAppState):
-    state = params.state
-    state_id = hashlib.md5(state.encode()).hexdigest()
-    server_cache.stat_app_state.save(value=state, key=[state_id])
+@app.post('/PushState')
+def push_state(params: ViewState):
+    state_id = view_state_cache.push_state(params.state)
     return get_response(json_data=state_id)
 
 
-@app.get('/pull_state')
+@app.get('/PullState')
 def pull_state(state_id: str, ):
-    state = server_cache.stat_app_state.get(state_id)
+    state = view_state_cache.pull_state(state_id)
     return get_response(json_data=state)
--- server_cache.py
+++
@@ -1,7 +0,0 @@
-from toolbox.cache.cache_object import CacheObject
-
-
-class ServerCache:
-
-    def __init__(self) -> None:
-        self.stat_app_state = CacheObject('stat_app_state', expire=None)
