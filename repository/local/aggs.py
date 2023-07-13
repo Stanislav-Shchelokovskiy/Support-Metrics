97a48d5c6fe8111dd0896c5b542383b3f109b4a5
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-13 17:49:16 +0400

    add metrics groups

--- repository/local/aggs.py
+++ repository/local/aggs.py
@@ -1,22 +1,28 @@
 from collections.abc import Callable, Mapping
-from toolbox.sql.aggs import Metric, COUNT_DISTINCT, COUNT, none_metric
+from toolbox.sql.aggs import Metric, COUNT_DISTINCT, COUNT, NONE_METRIC
 from sql_queries.meta import TicketsWithIterationsMeta
 
 
 people = Metric(
     'People',
+    'Activity',
     COUNT_DISTINCT(TicketsWithIterationsMeta.user_id),
 )
 tickets = Metric(
     'Tickets',
+    'Activity',
     COUNT_DISTINCT(TicketsWithIterationsMeta.ticket_scid),
 )
 iterations = Metric(
     'Iterations',
+    'Activity',
     COUNT(TicketsWithIterationsMeta.emp_post_id),
 )
-iterations_to_tickets = Metric.from_metric('Iterations / Tickets', iterations / tickets)
-
+iterations_to_tickets = Metric.from_metric(
+    'Iterations / Tickets',
+    'Activity',
+    iterations / tickets,
+)
 
 metrics = {
     people.name: people,
@@ -27,11 +33,13 @@ metrics = {
 
 
 def get_metric(metric: str) -> Metric:
-    return get_metrics().get(metric, none_metric)
+    return get_metrics().get(metric, NONE_METRIC)
 
 
-def get_metrics_names(formatter: Callable[[Metric], str] = lambda x: x.name) -> list[str]:
-    return [formatter(x) for x in get_metrics().values()]
+def get_metrics_projections(
+    projector: Callable[[Metric], str] = lambda x: x.name
+) -> list[str]:
+    return [projector(x) for x in get_metrics().values()]
 
 
 def get_metrics() -> Mapping[str, Metric]:
