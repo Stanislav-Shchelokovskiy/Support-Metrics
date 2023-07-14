3a47b12b4d5eb592605333e2d2c7f692a89dcadc
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-07-14 10:13:26 +0400

    support emps with multiple tents

--- sql_queries/transform_load/emps.sql
+++ sql_queries/transform_load/emps.sql
@@ -4,12 +4,14 @@ CREATE TABLE {Employees}(
   {tribe_id}      TEXT,
   {tent_id}       TEXT,
   {crmid}         TEXT,
-  {scid}          TEXT PRIMARY KEY,
+  {scid}          TEXT,
   {name}          TEXT,
   {tribe_name}    TEXT,
   {tent_name}     TEXT,
   {position_name} TEXT
-) WITHOUT ROWID;
+);
+
+CREATE INDEX idx_{scid}_{name} ON {Employees}({scid}, {name});
 
 CREATE INDEX idx_{Employees}_{position_id}_{tribe_id} ON {Employees}({position_id}, {tribe_id}, {scid}, {name});
 CREATE INDEX idx_{Employees}_{tribe_id} ON {Employees}({tribe_id}, {scid}, {name});
