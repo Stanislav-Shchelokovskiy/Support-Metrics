0ff7c6e5ba204f7a6705495db3ea42413a5a0041
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-11-08 17:49:32 +0400

    add type annotations for validation

--- server_models.py
+++ server_models.py
@@ -9,21 +9,21 @@ from toolbox.server_models import (
 
 
 class TentsParams(ServerModel):
-    tents: FilterParametersNode
+    tents: FilterParametersNode[str]
 
 
 class FeatureParams(TentsParams):
-    components: FilterParametersNode
+    components: FilterParametersNode[str]
 
 
 class EmployeeParams(ServerModel):
-    positions: FilterParametersNode
-    tribes: FilterParametersNode
-    tents: FilterParametersNode
+    positions: FilterParametersNode[str]
+    tribes: FilterParametersNode[str]
+    tents: FilterParametersNode[str]
 
 
 class ConversionStatusParams(ServerModel):
-    license_statuses: FilterParametersNode
+    license_statuses: FilterParametersNode[int]
 
 
 class CustomersParams(ServerModel):
@@ -32,7 +32,7 @@ class CustomersParams(ServerModel):
 
 class Percentile(ServerModel):
     metric: str
-    value: FilterParameterNode
+    value: FilterParameterNode[int]
 
     @validator('metric')
     def to_valid_literal(cls, v: str):
@@ -42,37 +42,37 @@ class Percentile(ServerModel):
 # yapf: disable
 class TicketsWithIterationsParams(ServerModel):
     percentile: Percentile = Field(alias='Percentile')
-    is_private: FilterParameterNode | None = Field(alias='Ticket visibility')
-    is_employee: FilterParameterNode | None = Field(alias='Ticket owner')
-    tribe_ids: FilterParametersNode | None = Field(alias='Tribes')
-    tent_ids: FilterParametersNode | None = Field(alias='Tents')
-    platforms_ids: FilterParametersNode | None = Field(alias='Platforms')
-    products_ids: FilterParametersNode | None = Field(alias='Products')
-    tickets_tags: FilterParametersNode | None = Field(alias='Ticket tags')
-    tickets_types: FilterParametersNode | None = Field(alias='Ticket types')
-    duplicated_to_tickets_types: FilterParametersNode | None = Field(alias='Duplicated to ticket types')
-    builds: FilterParametersNode | None = Field(alias='Versions')
-    fixed_in_builds: FilterParametersNode | None = Field(alias='Fixed In')
-    severity: FilterParametersNode | None = Field(alias='Severity')
-    ticket_status: FilterParametersNode | None = Field(alias='Ticket statuses')
-    operating_system_id: FilterParametersNode | None = Field(alias='Operating systems')
-    frameworks: FilterParametersNode | None = Field(alias='Frameworks/Specifics')
-    ide_id: FilterParametersNode | None = Field(alias='IDE')
-    customers_groups: FilterParametersNode | None = Field(alias='User groups')
-    license_statuses: FilterParametersNode | None = Field(alias='User types')
-    conversion_statuses: FilterParametersNode | None = Field(alias='User conversion types')
-    positions_ids: FilterParametersNode | None = Field(alias='Employees positions')
-    emp_tribe_ids: FilterParametersNode | None = Field(alias='Employees tribes')
-    emp_tent_ids: FilterParametersNode | None = Field(alias='Employees tents')
-    emp_ids: FilterParametersNode | None = Field(alias='Employees')
-    assigned_to_ids: FilterParametersNode | None = Field(alias='Assigned to')
-    closed_by_ids: FilterParametersNode | None = Field(alias='Closed by')
-    closed_between: FilterParametersNode | None = Field(alias='Closed', positive_filter_op='between', negative_filter_op='notbetween')
-    closed_for_n_days: FilterParameterNode | None = Field(alias='Closed for', positive_filter_op='>=', negative_filter_op='<')
-    fixed_by_ids: FilterParametersNode | None = Field(alias='Fixed by')
-    fixed_between: FilterParametersNode | None = Field(alias='Fixed', positive_filter_op='between', negative_filter_op='notbetween')
-    reply_ids: FilterParametersNode | None = Field(alias='CAT replies types')
-    components_ids: FilterParametersNode | None = Field(alias='CAT components')
-    feature_ids: FilterParametersNode | None = Field(alias='CAT features')
-    customers_crmids: FilterParametersNode | None = Field(alias='Customers')
+    is_private: FilterParameterNode[int] | None = Field(alias='Ticket visibility')
+    is_employee: FilterParameterNode[int] | None = Field(alias='Ticket owner')
+    tribe_ids: FilterParametersNode[str] | None = Field(alias='Tribes')
+    tent_ids: FilterParametersNode[str] | None = Field(alias='Tents')
+    platforms_ids: FilterParametersNode[str] | None = Field(alias='Platforms')
+    products_ids: FilterParametersNode[str] | None = Field(alias='Products')
+    tickets_tags: FilterParametersNode[str] | None = Field(alias='Ticket tags')
+    tickets_types: FilterParametersNode[int] | None = Field(alias='Ticket types')
+    duplicated_to_tickets_types: FilterParametersNode[int] | None = Field(alias='Duplicated to ticket types')
+    builds: FilterParametersNode[str] | None = Field(alias='Versions')
+    fixed_in_builds: FilterParametersNode[str] | None = Field(alias='Fixed In')
+    severity: FilterParametersNode[str] | None = Field(alias='Severity')
+    ticket_status: FilterParametersNode[str] | None = Field(alias='Ticket statuses')
+    operating_system_id: FilterParametersNode[str] | None = Field(alias='Operating systems')
+    frameworks: FilterParametersNode[str] | None = Field(alias='Frameworks/Specifics')
+    ide_id: FilterParametersNode[str] | None = Field(alias='IDE')
+    customers_groups: FilterParametersNode[str] | None = Field(alias='User groups')
+    license_statuses: FilterParametersNode[int] | None = Field(alias='User types')
+    conversion_statuses: FilterParametersNode[int] | None = Field(alias='User conversion types')
+    positions_ids: FilterParametersNode[str] | None = Field(alias='Employees positions')
+    emp_tribe_ids: FilterParametersNode[str] | None = Field(alias='Employees tribes')
+    emp_tent_ids: FilterParametersNode[str] | None = Field(alias='Employees tents')
+    emp_ids: FilterParametersNode[str] | None = Field(alias='Employees')
+    assigned_to_ids: FilterParametersNode[str] | None = Field(alias='Assigned to')
+    closed_by_ids: FilterParametersNode[str] | None = Field(alias='Closed by')
+    closed_between: FilterParametersNode[str] | None = Field(alias='Closed', positive_filter_op='between', negative_filter_op='notbetween')
+    closed_for_n_days: FilterParameterNode[int] | None = Field(alias='Closed for', positive_filter_op='>=', negative_filter_op='<')
+    fixed_by_ids: FilterParametersNode[str] | None = Field(alias='Fixed by')
+    fixed_between: FilterParametersNode[str] | None = Field(alias='Fixed', positive_filter_op='between', negative_filter_op='notbetween')
+    reply_ids: FilterParametersNode[str] | None = Field(alias='CAT replies types')
+    components_ids: FilterParametersNode[str] | None = Field(alias='CAT components')
+    feature_ids: FilterParametersNode[str] | None = Field(alias='CAT features')
+    customers_crmids: FilterParametersNode[str] | None = Field(alias='Customers')
 # yapf: enable
