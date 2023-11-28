a4fd289d7c66ed23564e62c4ca68be007e5958f8
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-11-28 14:50:05 +0400

    fix roles query

--- sql_queries/remote/roles.sql
+++ sql_queries/remote/roles.sql
@@ -2,8 +2,8 @@ DECLARE @employees VARCHAR(MAX) = N'{employees_json}'
 
 SELECT DISTINCT roles.value AS {id},
                 roles.value AS {name} 
-FROM    DXStatisticsV2.dbo.parse_employees(@employees)
+FROM    DXStatisticsV2.dbo.parse_employees(@employees) AS e
         OUTER APPLY (
             SELECT value
-            FROM STRING_SPLIT(ds.roles, ';')
+            FROM STRING_SPLIT(e.roles, ';')
         ) AS roles
