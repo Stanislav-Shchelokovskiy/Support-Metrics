36fb49b066752fc893a42d0ede921bb9bc95f56f
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-11-29 11:04:17 +0400

    format roles names

--- sql_queries/remote/roles.sql
+++ sql_queries/remote/roles.sql
@@ -1,7 +1,7 @@
 DECLARE @employees VARCHAR(MAX) = N'{employees_json}'
 
-SELECT DISTINCT roles.value AS {id},
-                roles.value AS {name} 
+SELECT DISTINCT roles.value                 AS {id},
+                TRIM('()' FROM roles.value) AS {name} 
 FROM    DXStatisticsV2.dbo.parse_employees(@employees) AS e
         OUTER APPLY (
             SELECT value
