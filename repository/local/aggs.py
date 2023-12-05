117845797cbb7411f7a1978257395c46c6f88e87
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2023-12-05 16:50:36 +0400

    reorganize metrics

--- repository/local/aggs.py
+++ repository/local/aggs.py
@@ -2,42 +2,43 @@ from collections.abc import Callable, Mapping
 from toolbox.sql.aggs import Metric, COUNT_DISTINCT, COUNT, SUM, NONE_METRIC
 from sql_queries.meta.aggs import TicketsWithIterations, CSI
 
-
 people = Metric(
     'People',
     '',
-    'Activity',
+    'Entities',
     COUNT_DISTINCT(TicketsWithIterations.user_id),
 )
 tickets = Metric(
     'Tickets',
     '',
-    'Activity',
+    'Entities',
     COUNT_DISTINCT(TicketsWithIterations.ticket_scid),
 )
 iterations = Metric(
     'Iterations',
     'Replies',
-    'Activity',
+    'Entities',
     COUNT(TicketsWithIterations.emp_post_id),
 )
+
 iterations_to_tickets = Metric.from_metric(
     'Iterations / Tickets',
     'Replies Rate',
-    'Activity',
+    'Productivity',
     iterations / tickets,
 )
+
 csi = Metric(
     'Satisfaction Index',
     'Customer Satisfaction Index',
-    'Activity',
+    'Customers',
     SUM(f'IIF({CSI.rating} = 1, 1, 0)') / COUNT('*') * 100,
 )
 
 metrics = {
+    iterations.name: iterations,
     people.name: people,
     tickets.name: tickets,
-    iterations.name: iterations,
     iterations_to_tickets.name: iterations_to_tickets,
     csi.name: csi,
 }
