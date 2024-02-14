e998b533c897df11035e1850d19014e092482841
Stanislav Shchelokovskiy <stanislav.shchelokovskiy@gmail.com>
2024-02-14 14:25:17 +0400

    bug resolution time: use either date or creation date

--- sql_queries/remote/metrics/resolution_time.sql
+++ sql_queries/remote/metrics/resolution_time.sql
@@ -12,19 +12,30 @@ WHERE	ticket_type != @bug
 GROUP BY ticket_scid
 UNION
 SELECT	tickets.FriendlyId,
-		DATEDIFF(HOUR, tickets.Created, ISNULL(fixed_info.fixed_on, closed_info.closed_on)),
-		DATEDIFF(HOUR, tickets.Created, ISNULL(fixed_info.fixed_on, closed_info.closed_on))
+		DATEDIFF(HOUR, conversion_to_bug.happened_on, ISNULL(fixed_info.fixed_on, closed_info.closed_on)),
+		DATEDIFF(HOUR, conversion_to_bug.happened_on, ISNULL(fixed_info.fixed_on, closed_info.closed_on))
 FROM   	SupportCenterPaid.[c1f0951c-3885-44cf-accb-1a390f34c342].Tickets AS tickets
-		OUTER APPLY (
-			SELECT	 TOP 1 AuditOwner AS closed_by, EntityModified AS closed_on
-			FROM	 scpaid_audit.[c1f0951c-3885-44cf-accb-1a390f34c342].scworkflow_TicketProperties
-			WHERE	 Ticket_Id = tickets.Id AND Name = 'TicketStatus' AND Value = 'Closed'
+        OUTER APPLY (
+			SELECT TOP 1 Modified AS happened_on
+			FROM 	scpaid_audit.[c1f0951c-3885-44cf-accb-1a390f34c342].scworkflow_Tickets
+			WHERE 	AuditAction IN (0 /* Insert */, 1 /* Update */)
+				AND EntityOid = tickets.Id
+				AND	ChangedProperties LIKE '%EntityType%'
+				AND	EntityType = @bug
+            ORDER BY AuditAction DESC, happened_on DESC
+		) AS conversion_to_bug
+		CROSS APPLY (
+			SELECT  TOP 1 EntityModified AS closed_on
+			FROM    scpaid_audit.[c1f0951c-3885-44cf-accb-1a390f34c342].scworkflow_TicketProperties
+			WHERE   Ticket_Id = tickets.Id AND Name = 'TicketStatus' AND Value = 'Closed'
+                AND EntityModified >= conversion_to_bug.happened_on
 			ORDER BY EntityModified DESC
 		) AS closed_info
 		OUTER APPLY (
-			SELECT	 TOP 1 AuditOwner AS fixed_by, EntityModified AS fixed_on
-			FROM	 scpaid_audit.[c1f0951c-3885-44cf-accb-1a390f34c342].scworkflow_TicketProperties
-			WHERE	 Ticket_Id = tickets.Id AND Name = 'FixedInBuild'
+			SELECT	TOP 1 EntityModified AS fixed_on
+			FROM	scpaid_audit.[c1f0951c-3885-44cf-accb-1a390f34c342].scworkflow_TicketProperties
+			WHERE	Ticket_Id = tickets.Id AND Name = 'FixedInBuild'
+                AND EntityModified >= conversion_to_bug.happened_on
 			ORDER BY EntityModified DESC
 		) AS fixed_info
 WHERE 	Created BETWEEN @start AND @end
